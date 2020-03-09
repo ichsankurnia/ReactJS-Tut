e1e82f3be9fcbefac85d8019e26b25ebd7ec214b
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2020-03-09 17:48:47 +0700

    change state into constructor method

--- src/containers/Pages/Token/HomeToken.jsx
+++ src/containers/Pages/Token/HomeToken.jsx
@@ -3,8 +3,11 @@ import React, { Component, Fragment } from 'react'
 import axios from "axios";
 
 class HomeToken extends Component{
-    state = {
-        data: []
+    constructor(props){
+        super(props)
+        this.state = {
+            data: []
+        }
     }
 
     componentDidMount(){

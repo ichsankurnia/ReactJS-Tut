09b9f854c0f7b13076ee07e7e3aa509dbf793462
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2020-03-14 07:42:37 +0700

    add class to store harcode value there

---
+++ src/containers/Pages/Token/Variabels.js
@@ -0,0 +1,17 @@
+export class Variable{
+    constructor(lang, slug){
+        this.lang = lang
+        this.slug = slug
+    }
+
+    getDescEKTP = () => {
+        let desc1, desc2, titlebuton, title
+        if(this.lang === "id"){
+            desc1 = 'Letakkan e-KTP Anda pada Alat Pemindai'; desc2 = `Lalu Tekan Tombol "Pindai e-KTP"`; titlebuton='Pindai e-KTP'
+        }else{
+            desc1 = 'Put your e-KTP on the e-KTP Reader'; desc2 = 'Then press the "Scan e-KTP" button'; titlebuton='Scan e-KTP' 
+        }
+
+        return {desc1: desc1, desc2: desc2, titlebuton: titlebuton}
+    }
+}

03229e47e13cde4b7e924c15656c5c7fdff45561
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2020-03-21 13:40:19 +0700

    generate correct bill and correct phone number

--- src/containers/Pages/Token/Params.jsx
+++ src/containers/Pages/Token/Params.jsx
@@ -15,14 +15,23 @@ export default class Params extends React.Component {
             desc2: "",
             phoneNumber: "",
             transaksiID: "",
+            billAmount: genTxID.generateCorrectBill(params.bill)
         }
         this.variable = new Variable(this.state.lang, this.state.slug).getDescEKTP()
     }
 
     componentDidMount() {
-        this.setState({lang: this.state.lang, slug: this.state.slug, desc1: this.variable.desc1, desc2: this.variable.desc2, phoneNumber: GetSetter.phoneNumber}, () => {
-            let txId = genTxID.generateTXID(this.state.phoneNumber)
-            this.setState({transaksiID: txId}, () => console.log(this.state.transaksiID.length))
+        let pN = GetSetter.phoneNumber
+        if (pN.charAt(0) === "0"){
+            pN = "62" + (pN.substring(1, pN.length))
+        }
+        this.setState({
+            desc1: this.variable.desc1,
+            desc2: this.variable.desc2,
+            phoneNumber: pN,
+        }, () => {
+                let txId = genTxID.generateTXID(this.state.phoneNumber)
+                this.setState({transaksiID: txId}, () => console.log(this.state.transaksiID.length))
         })
     }
 
@@ -34,6 +43,7 @@ export default class Params extends React.Component {
                 <p>Slugify: {this.state.slug}</p>
                 <p>Phone Number: {this.state.phoneNumber}</p>
                 <p>Transaksi ID: {this.state.transaksiID}</p>
+                <p>Bill Amount: {this.state.billAmount}</p>
                 <p>{this.state.desc1}</p>
                 <p>{this.state.desc2}</p>
                 <button onClick={() => this.props.history.goBack()}>Back</button>

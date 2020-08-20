360ac512a272cab62947c97bb767ab4927b159dc
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2020-08-20 21:36:38 +0700

    create provider and store to apps

--- src/index.js
+++ src/index.js
@@ -3,5 +3,16 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 import Home from './containers/Home';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './Redux/Reducer/globalReducer';
 
-ReactDOM.render(<Home />, document.getElementById('root'));
+
+/* STORE */
+const store = createStore(rootReducer)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Home />
+    </Provider>,
+document.getElementById('root'));

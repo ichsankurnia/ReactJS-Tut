e957d94249b58ad958ccaf7db41a8bb99899cad4
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2020-10-05 14:39:43 +0700

    Create initialState.js

---
+++ src/Redux/Reducer/initialState.js
@@ -0,0 +1,17 @@
+const initialState = {
+    totalOrder: 0,
+    testState: 'jhjas',
+    ektp: {
+
+    },
+    psbState: {
+        pattern: "",
+        msisdn: ""
+    },
+    pspState: {
+
+    }
+}
+
+export default initialState
+
